NVIDIA/bare-metal-manager-core

feat: refine expected racks implementation
This introduces an `ExpectedRacks` management flow, where an expected rack contains:
- The `rack_id` we expect.
- The `rack_type`, which is how we know the number of compute trays, NVLink switches, and power shelves.

I'm introducing a basic/introductory `RackTypeConfig` here, but it can (and probably will) iterate over time.

From there, nodes continue to register for a given `rack_id` as they have been doing, but I moved that logic into `db_rack`, and we now have:
- `register_expected_machine`
- `register_expected_power_shelf`
- `register_expected_switch`.

The behavior is the same as it was, but to me it reads a little bit better to see that a node is registering "into" a rack.

This then wires up the `Rack` state controller to look at it's `rack_type` to check if:
- The number of expected nodes of each type have registered into the rack.
- The nodes are linked.

This does NOT:
- Take into account node/tray position in the rack, which is a TODO.
- Introduce any revamping of the rack state controller states, which is also a TODO.

However, I did want to start getting this basic plumbing in place for us to iterate against, hopefully in a way that isn't super disruptive and is something we feel comfortable dropping in + continuing to build on.

Some choices I *DID* have to make here, though, which we could change now, are:
1. A node will still "initialize" an empty `Rack` if the `rack_id` doesn't exist yet (just like it does today), but with `rack_type: None`. This allow nodes to start populating a `RackConfig` with nodes even if the `ExpectedRack` doesn't exist yet. Once the `ExpectedRack` comes in, we'll find the matching `rack_id`, see that it has `rack_type: None`, and basically adopt it by giving it a `rack_type`. If it already has a `rack_type`, that means it's effectively been adopted and we'll error, and require the user to "update" the rack.
2. For the state controller, if the `rack_type=None` (and we haven't adopt

crates/api-model/src/rack.rs
@@ -71,7 +71,12 @@ impl From<Rack> for rpc::forge::Rack {
                 .iter()
                 .map(|x| x.to_string())
                 .collect(),
-            expected_nvlink_switches: vec![],
+            expected_nvlink_switches: value
+                .config
+                .expected_switches
+                .iter()
+                .map(|x| x.to_string())
+                .collect(),
             compute_trays: value.config.compute_trays,
             power_shelves: value.config.power_shelves,
             created: Some(Timestamp::from(value.created)),
@@ -245,15 +250,27 @@ pub struct RackStateHistory {
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct RackConfig {
     pub compute_trays: Vec<MachineId>,
-    // todo: put in nvlink switch ids here when that code lands
-    // pub nvlink_switches: Vec<NvlinkSwitchId>
     pub power_shelves: Vec<PowerShelfId>,
 
-    // store bmc mac address of every tray in the rack
+    /// expected_compute_trays contains the BMC MAC addresses of every
+    /// expected compute tray in the rack.
     pub expected_compute_trays: Vec<MacAddress>,
-    // todo: nvlink switches
-    // pub expected_nvlink_switches: Vec<MacAddress>,
+    /// expected_switches contains the BMC MAC addresses of every expected
+    /// switch in the rack. The NVOS management MAC address is stored
+    /// separately in the expected switch's metadata labels, and validated
+    /// separately as part of the switch state controller.
+    #[serde(default)]
+    pub expected_switches: Vec<MacAddress>,
+    /// expected_power_shelves contains the BMC MAC addresses of every
+    /// expected power shelf in the rack.
     pub expected_power_shelves: Vec<MacAddress>,
+
+    /// rack_type_definition is the full rack type definition embedded at
+    /// the time the expected rack is created or updated. This avoids
+    /// runtime lookups against the config file and ensures the rack
+    /// retains its definition even if the config changes later.
+    #[serde(default)]
+    pub rack_type_definition: Option<crate::rack_type::RackTypeDefinition>,
 }
 
 pub fn state_sla(state: &RackState, state_version: &ConfigVersion) -> StateSla {

crates/api/src/tests/common/api_fixtures/site_explorer.rs
@@ -1566,6 +1566,7 @@ pub struct TestRackDbBuilder {
     expected_power_shelves: Vec<MacAddress>,
     expected_switches: Vec<MacAddress>,
     rack_id: RackId,
+    rack_type_definition: Option<model::rack_type::RackTypeDefinition>,
 }
 
 impl Default for TestRackDbBuilder {
@@ -1575,6 +1576,7 @@ impl Default for TestRackDbBuilder {
             expected_power_shelves: vec![],
             expected_switches: vec![],
             rack_id: RackId::from(uuid::Uuid::new_v4()),
+            rack_type_definition: None,
         }
     }
 }
@@ -1607,6 +1609,19 @@ impl TestRackDbBuilder {
         self
     }
 
+    pub fn with_expected_switches(mut self, expected_switches: Vec<[u8; 6]>) -> Self {
+        self.expected_switches = expected_switches.into_iter().map(MacAddress::new).collect();
+        self
+    }
+
+    pub fn with_rack_type_definition(
+        mut self,
+        rack_type_def: model::rack_type::RackTypeDefinition,
+    ) -> Self {
+        self.rack_type_definition = Some(rack_type_def);
+        self
+    }
+
     pub async fn persist(&self, txn: &mut PgConnection) -> Result<RackId, DatabaseError> {
         db_rack::create(
             txn,
@@ -1622,7 +1637,9 @@ impl TestRackDbBuilder {
             compute_trays: vec![],
             power_shelves: vec![],
             expected_compute_trays: self.expected_compute_trays.clone(),
+            expected_switches: self.expected_switches.clone(),
             expected_power_shelves: self.expected_power_shelves.clone(),
+            rack_type_definition: self.rack_type_definition.clone(),
         };
 
         db_rack::update(txn, self.rack_id, &cfg).await?;

sabbanis/ncx-infra-controller-core

crates/api-model/src/rack.rs
@@ -71,7 +71,12 @@ impl From<Rack> for rpc::forge::Rack {
                 .iter()
                 .map(|x| x.to_string())
                 .collect(),
-            expected_nvlink_switches: vec![],
+            expected_nvlink_switches: value
+                .config
+                .expected_switches
+                .iter()
+                .map(|x| x.to_string())
+                .collect(),
             compute_trays: value.config.compute_trays,
             power_shelves: value.config.power_shelves,
             created: Some(Timestamp::from(value.created)),
@@ -245,15 +250,27 @@ pub struct RackStateHistory {
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct RackConfig {
     pub compute_trays: Vec<MachineId>,
-    // todo: put in nvlink switch ids here when that code lands
-    // pub nvlink_switches: Vec<NvlinkSwitchId>
     pub power_shelves: Vec<PowerShelfId>,
 
-    // store bmc mac address of every tray in the rack
+    /// expected_compute_trays contains the BMC MAC addresses of every
+    /// expected compute tray in the rack.
     pub expected_compute_trays: Vec<MacAddress>,
-    // todo: nvlink switches
-    // pub expected_nvlink_switches: Vec<MacAddress>,
+    /// expected_switches contains the BMC MAC addresses of every expected
+    /// switch in the rack. The NVOS management MAC address is stored
+    /// separately in the expected switch's metadata labels, and validated
+    /// separately as part of the switch state controller.
+    #[serde(default)]
+    pub expected_switches: Vec<MacAddress>,
+    /// expected_power_shelves contains the BMC MAC addresses of every
+    /// expected power shelf in the rack.
     pub expected_power_shelves: Vec<MacAddress>,
+
+    /// rack_capabilities is the full rack capabilities set embedded at
+    /// the time the expected rack is created or updated. This avoids
+    /// runtime lookups against the config file and ensures the rack
+    /// retains its capabilities even if the config changes later.
+    #[serde(default)]
+    pub rack_capabilities: Option<crate::rack_type::RackCapabilitiesSet>,
 }
 
 pub fn state_sla(state: &RackState, state_version: &ConfigVersion) -> StateSla {

crates/api/src/tests/common/api_fixtures/site_explorer.rs
@@ -1513,6 +1513,7 @@ pub struct TestRackDbBuilder {
     expected_power_shelves: Vec<MacAddress>,
     expected_switches: Vec<MacAddress>,
     rack_id: RackId,
+    rack_capabilities: Option<model::rack_type::RackCapabilitiesSet>,
 }
 
 impl Default for TestRackDbBuilder {
@@ -1522,6 +1523,7 @@ impl Default for TestRackDbBuilder {
             expected_power_shelves: vec![],
             expected_switches: vec![],
             rack_id: RackId::from(uuid::Uuid::new_v4()),
+            rack_capabilities: None,
         }
     }
 }
@@ -1554,6 +1556,19 @@ impl TestRackDbBuilder {
         self
     }
 
+    pub fn with_expected_switches(mut self, expected_switches: Vec<[u8; 6]>) -> Self {
+        self.expected_switches = expected_switches.into_iter().map(MacAddress::new).collect();
+        self
+    }
+
+    pub fn with_rack_capabilities(
+        mut self,
+        rack_capabilities: model::rack_type::RackCapabilitiesSet,
+    ) -> Self {
+        self.rack_capabilities = Some(rack_capabilities);
+        self
+    }
+
     pub async fn persist(&self, txn: &mut PgConnection) -> Result<RackId, DatabaseError> {
         db_rack::create(
             txn,
@@ -1569,7 +1584,9 @@ impl TestRackDbBuilder {
             compute_trays: vec![],
             power_shelves: vec![],
             expected_compute_trays: self.expected_compute_trays.clone(),
+            expected_switches: self.expected_switches.clone(),
             expected_power_shelves: self.expected_power_shelves.clone(),
+            rack_capabilities: self.rack_capabilities.clone(),
         };
 
         db_rack::update(txn, self.rack_id, &cfg).await?;